f9c5995268c2c1df387bb9d32715b0c9e1aaf7c0
statios <statios@kakao.com>
2021-02-08 10:27:12 +0900

    Create a project

---
+++ Unsplashrimp/New Group/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Unsplashrimp
+//
+//  Created by KIHYUN SO on 2021/02/08.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    
+    return true
+  }
+
+}
+
---
+++ Unsplashrimp/New Group/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplashrimp
+//
+//  Created by KIHYUN SO on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
