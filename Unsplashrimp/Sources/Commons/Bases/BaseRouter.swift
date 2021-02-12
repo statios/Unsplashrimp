096264600553a63328de6a4485e52a6bd87ed0fb
statios <statios@kakao.com>
2021-02-12 16:47:48 +0900

    Remove router type

--- Unsplashrimp/Sources/Commons/Bases/BaseRouter.swift
+++ Unsplashrimp/Sources/Commons/Bases/BaseRouter.swift
@@ -8,12 +8,52 @@
 import Foundation
 import UIKit
 
-class BaseRouter: RouterType {
+class BaseRouter {
   init() {
     Log.verbose(String(describing: Self.self))
   }
   
   deinit {
     Log.verbose(String(describing: Self.self))
+  }
+  
+  func pop(
+    from target: UIViewController?,
+    animated: Bool = true
+  ) {
+    DispatchQueue.main.async {
+      target?.navigationController?.popViewController(animated: animated)
+    }
+  }
+  
+  func push(
+    to: UIViewController,
+    from target: UIViewController?,
+    animated: Bool = true
+  ) {
+    DispatchQueue.main.async {
+      target?.navigationController?.pushViewController(to, animated: animated)
+    }
+  }
+  
+  func present(
+    to: UIViewController,
+    from target: UIViewController?,
+    animated: Bool = true,
+    completion: (() -> Void)? = nil
+  ) {
+    DispatchQueue.main.async {
+      target?.present(to, animated: animated, completion: completion)
+    }
+  }
+  
+  func dismiss(
+    from target: UIViewController?,
+    animated: Bool = true,
+    completion: (() -> Void)? = nil
+  ) {
+    DispatchQueue.main.async {
+      target?.dismiss(animated: animated, completion: completion)
+    }
   }
 }
