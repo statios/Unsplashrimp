5b6ad9ae53056e8f30131f0a5fea27b8b0f49160
statios <statios@kakao.com>
2021-02-14 07:28:50 +0900

    Refactor in appDelegate

--- Unsplashrimp/Sources/Application/AppDelegate.swift
+++ Unsplashrimp/Sources/Application/AppDelegate.swift
@@ -9,17 +9,22 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
   var window: UIWindow?
   
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    UITextField.appearance().keyboardAppearance = UIKeyboardAppearance.dark
-    UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    setup(application)
     return true
   }
+}
 
+extension AppDelegate {
+  func setup(_ application: UIApplication) {
+    UITextField.appearance().keyboardAppearance = UIKeyboardAppearance.dark
+    UINavigationBar.appearance().titleTextAttributes = [
+      NSAttributedString.Key.foregroundColor: UIColor.white
+    ]
+  }
 }
