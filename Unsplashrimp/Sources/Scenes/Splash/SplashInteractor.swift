6aa5199c8b5777c5cee17de10996010523903103
statios <statios@kakao.com>
2021-02-14 13:46:20 +0900

    Add help methods in splash interactor

--- Unsplashrimp/Sources/Scenes/Splash/SplashInteractor.swift
+++ Unsplashrimp/Sources/Scenes/Splash/SplashInteractor.swift
@@ -29,24 +29,16 @@ final class SplashInteractor: BaseInteractor, SplashDataStore {
 // MARK: - Business Logic
 extension SplashInteractor: SplashBusinessLogic {
   func fetchPrefetch(request: SplashModels.Prefetch.Request) {
-    networkWorker?.request(
-      UnsplashAPI.topics,
-      type: [Topic].self) { [weak self] in
-      switch $0 {
-      case let .success(topics):
-        self?.topics = topics
-        self?.photos = topics.map { _ in [Photo]() }
-        topics.forEach {
-          self?.prefetchGroup.enter()
-          self?.fetchPhotos($0)
-        }
-        self?.prefetchGroup.notify(queue: .main) {
-          Log.debug("Prefetch Done")
-          self?.presenter?.presentPrefetch(response: .init())
-        }
-      case .failure:
-        // Do NOTHING
-        return
+    requestTopics { [weak self] in
+      self?.topics = $0
+      self?.photos = $0.map { _ in [Photo]() }
+      $0.forEach {
+        self?.prefetchGroup.enter()
+        self?.fetchPhotos($0)
+      }
+      self?.prefetchGroup.notify(queue: .main) {
+        Log.debug("Prefetch Done")
+        self?.presenter?.presentPrefetch(response: .init())
       }
     }
   }
@@ -54,20 +46,51 @@ extension SplashInteractor: SplashBusinessLogic {
 
 // MARK:- Helpers
 extension SplashInteractor {
-  private func fetchPhotos(_ topic: Topic) {
-    self.networkWorker?.request(
-      UnsplashAPI.photos(id: topic.id, page: 1),
-      type: [Photo].self
-    ) { [weak self] in
+  private func requestTopics(
+    onSuccess: @escaping ([Topic]) -> Void
+  ) {
+    networkWorker?.request(
+      UnsplashAPI.topics,
+      type: UnsplashResponse<[Topic]>.self
+    ) {
       switch $0 {
-      case let .success(photos):
-        let index = self?.topics.enumerated()
-          .filter { $0.element.id == topic.id }
-          .first?.offset ?? 0
-        self?.photos[index] = photos
-        self?.prefetchGroup.leave()
-      default: return
+      case let .success(response):
+        guard let successResult = response.successResult else { return }
+        onSuccess(successResult)
+      case .failure:
+        // Do NOTHING
+        return
       }
+    }
+  }
+  
+  private func requestPhotos(
+    id: String,
+    page: Int,
+    onSuccess: @escaping ([Photo]) -> Void
+  ) {
+    networkWorker?.request(
+      UnsplashAPI.photos(id: id, page: page),
+      type: UnsplashResponse<[Photo]>.self
+    ) {
+      switch $0 {
+      case let .success(response):
+        guard let successResult = response.successResult else { return }
+        onSuccess(successResult)
+      case .failure:
+        // Do NOTHING
+        return
+      }
+    }
+  }
+  
+  private func fetchPhotos(_ topic: Topic) {
+    requestPhotos(id: topic.id, page: 1) { [weak self] in
+      let index = self?.topics.enumerated()
+        .filter { $0.element.id == topic.id }
+        .first?.offset ?? 0
+      self?.photos[index] = $0
+      self?.prefetchGroup.leave()
     }
   }
 }
